2cd5f9f2f11d29f70664c9118a55f219b8c6c534
Sabrina <li.chidori.kinomoto@gmail.com>
2026-08-31 12:34:56 +0200

    essai de style pour formulaire

--- src/pages/LoginUser.tsx
+++ src/pages/LoginUser.tsx
@@ -17,7 +17,6 @@ function LoginUser() {
     const { login } = context;
 
     async function onSubmit(data) {
-        console.log(data);
         try {
             const response = await fetch(
                 `http://localhost:3000/users?email=${encodeURIComponent(data.email)}&password=${encodeURIComponent(data.password)}`
@@ -66,27 +65,35 @@ function LoginUser() {
     }
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label className="block text-sm font-medium text-black">Email</label>
-                    <input type="email"
-                        className="block rounded-md bg-white border border:bg-[#A0AAAB]"
-                        {...register("email",
-                            { required: "L'email est obligatoire", })} />
-                    {errors.email && (<p>{errors.email.message}</p>)}
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-black">Mot de passe</label>
-                    <input type="password"
-                        className="block rounded-md bg-white border border:bg-[#A0AAAB]"
-                        {...register("password",
-                            { required: "Le mot de passe est obligatoire", })} />
-                    {errors.password && (<p>{errors.password.message}</p>)}
-                </div>
-                <button type="submit"
-                    className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white"
-                > Se connecter </button>
-            </form>
+            <main className="min-h-screen bg-[#BCCCDB] flex flex-col items-center">
+                <h1 className="mt-14 text-center text-4xl font-extrabold uppercase text-white">
+                    Page de connexion
+                </h1>
+                <form onSubmit={handleSubmit(onSubmit)}
+                    className="mt-8 flex w-full max-w-[295px] flex-col">
+                    <div>
+                        {/* <label className="block text-sm font-medium text-black">Email</label> */}
+                        <input type="email"
+                            className="h-[30px] w-full rounded-md border border:bg-[#A0AAAB] border-2 bg-white"
+                            placeholder="identifiants ou adresse mail"
+                            {...register("email",
+                                { required: "L'email est obligatoire", })} />
+                        {errors.email && (<p>{errors.email.message}</p>)}
+                    </div>
+                    <div>
+                        {/* <label className="block text-sm font-medium text-black">Mot de passe</label> */}
+                        <input type="password"
+                            className="h-[30px] w-full rounded-md border border:bg-[#A0AAAB] border-2 bg-white"
+                            placeholder="Mot de passe"
+                            {...register("password",
+                                { required: "Le mot de passe est obligatoire", })} />
+                        {errors.password && (<p>{errors.password.message}</p>)}
+                    </div>
+                    <button type="submit"
+                        className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white"
+                    > Se connecter </button>
+                </form>
+            </main>
         </>
     )
 }
