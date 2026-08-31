ffd56eb5dd516b5b781b8c0461465921f89cdbaa
Martin Mabrut <martinmabrut43@gmail.com>
2026-08-31 14:15:53 +0200

    Fomulaire terminé

--- src/composants/UpdateReservation.tsx
+++ src/composants/UpdateReservation.tsx
@@ -35,10 +35,43 @@ function UpdateReservation() {
         });
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        setReservation({
+            "id": reservation.id,
+            "salle_id": form.salle_id,
+            "date_debut": form.date_debut,
+            "date_fin": form.date_fin,
+            "user_id": reservation.user_id
+        });
+    }
+
     function postReservation() {
     }
 
 return  <div>
+            <form onSubmit={handleSubmit}>
+
+                <div>
+                    <label>Salle</label>
+                    <input name = "salle_id" value={form.salle_id} onChange={handleChange}/>
+                </div>
+
+                <div>
+                    <label>Date de début</label>
+                    <input name = "date_debut" value={form.date_debut} onChange={handleChange}/>
+                </div>
+
+                <div>
+                    <label>Date de fin</label>
+                    <input name = "date_fin" value={form.date_fin} onChange={handleChange}/>
+                </div>
+
+                <button type="submit">Modifier</button>
+                <button>Annuler</button>
+
+            </form>
         </div>
 
 };
