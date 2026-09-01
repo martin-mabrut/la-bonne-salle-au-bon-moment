ccc7493f0859484be158c79e347fcffde0438713
Sabrina <li.chidori.kinomoto@gmail.com>
2026-09-01 16:24:11 +0200

    mise en place de la logique de récupération des utilisateurs

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,7 +1,20 @@
+import { useContext, useEffect } from "react";
+import { UserContext } from "../context/UserContext";
+
 function UserList() {
+    const { getUserList, userList } = useContext(UserContext);
+    useEffect(() => { getUserList() }, [])
+
     return (
 
         <>
+
+            {userList.map((user) => (
+                <p key={user.id}>
+                    {user.lastname} {user.firstname} {user.email}
+                </p>
+            ))}
+
         </>
     )
 }
