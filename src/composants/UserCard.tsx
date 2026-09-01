5986326508dddbc6025fa1cf4860751700ac630b
Sabrina <li.chidori.kinomoto@gmail.com>
2026-09-01 16:46:12 +0200

    finalisation

--- src/composants/UserCard.tsx
+++ src/composants/UserCard.tsx
@@ -13,12 +13,16 @@ interface UserCardProps {
 
 function UserCard({ user }: UserCardProps) {
     return (
+        <>
 
-        <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-4 shadow-sm">
-            <p className="text-sm text-gray-500">Prénom : <span className="font-medium text-gray-900">{user.firstname}</span></p>
-            <p className="text-sm text-gray-500">Nom : <span className="font-medium text-gray-900">{user.lastname}</span></p>
-            <p className="text-sm text-gray-500">Email : <span className="font-medium text-gray-900">{user.email}</span></p>
-        </div>
+            <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-4 shadow-sm">
+                <p className="text-sm text-gray-500">Prénom : <span className="font-medium text-gray-900">{user.firstname}</span></p>
+                <p className="text-sm text-gray-500">Nom : <span className="font-medium text-gray-900">{user.lastname}</span></p>
+                <p className="text-sm text-gray-500">Email : <span className="font-medium text-gray-900">{user.email}</span></p>
+            </div>
+
+        </>
+
     );
 }
 
